c6a110ad81afc93fc9e7504b88ebec3b30f10afe
KisaragiEffective <kisaragi.effective@gmail.com>
2019-08-31 00:40:03 +0900

    generate

---
+++ generator/DestructionValueProtocolGenerator.kts
@@ -0,0 +1,14 @@
+println("""interface DestructionValueProtocol""")
+println()
+println("""interface DestructionValueProtocol1<E1> : DestructionValueProtocol {
+    |    fun component1(): E1
+    |}
+""".trimMargin())
+println()
+
+for (i in 2..32) {
+    println("interface DestructionValueProtocol$i<" + (1..i).joinToString { "E$it" } + "> : DestructionValueProtocol${i - 1}<" + (1..(i-1)).joinToString { "E$it" } + "> {")
+    println("    fun component$i(): E$i")
+    println("}")
+    println()
+}
