410716a9674a23712a206d30e7ff8f7e53c89d67
KisaragiEffective <kisaragi.effective@gmail.com>
2019-08-30 23:34:04 +0900

    update

--- generator/JagArrayRWGenerator.kts
+++ generator/JagArrayRWGenerator.kts
@@ -1,3 +1,6 @@
 for (i in 2..32) {
+    println("import com.github.kisaragieffective.kisaragistd.JagArray${i}D")
+}
+for (i in 2..32) {
     println("operator fun <E> JagArray${i}D<E>.get(" + (1..i).joinToString { "i$it: Int" } + ") = " + (1..i).joinToString{ "[i$it]" })
 }
