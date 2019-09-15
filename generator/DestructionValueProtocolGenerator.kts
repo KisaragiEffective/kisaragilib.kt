65344ff26fea7f955d1c5cdc0140a35342274785
KisaragiEffective <kisaragi.effective@gmail.com>
2019-09-15 17:01:01 +0900

    update variance

--- generator/DestructionValueProtocolGenerator.kts
+++ generator/DestructionValueProtocolGenerator.kts
@@ -1,13 +1,13 @@
 println("""interface DestructionValueProtocol""")
 println()
-println("""interface DestructionValueProtocol1<E1> : DestructionValueProtocol {
+println("""interface DestructionValueProtocol1<out E1> : DestructionValueProtocol {
     |    fun component1(): E1
     |}
 """.trimMargin())
 println()
 
 for (i in 2..32) {
-    println("interface DestructionValueProtocol$i<" + (1..i).joinToString { "E$it" } + "> : DestructionValueProtocol${i - 1}<" + (1..(i-1)).joinToString { "E$it" } + "> {")
+    println("interface DestructionValueProtocol$i<" + (1..i).joinToString { "out E$it" } + "> : DestructionValueProtocol${i - 1}<" + (1..(i-1)).joinToString { "E$it" } + "> {")
     println("    fun component$i(): E$i")
     println("}")
     println()
