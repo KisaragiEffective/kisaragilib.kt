6ab3c3d9e96688e0c2bd5208fda7b8d89e50e7f5
KisaragiEffective <kisaragi.effective@gmail.com>
2019-10-25 02:13:05 +0900

    +typealias

---
+++ src/main/java/com/github/kisaragieffective/kisaragistd/TypeAliases.kt
@@ -0,0 +1,17 @@
+package com.github.kisaragieffective.kisaragistd
+
+typealias i1 = Boolean
+typealias i8 = Byte
+typealias i16 = Short
+typealias i32 = Int
+typealias i64 = Long
+typealias f32 = Float
+typealias f64 = Double
+typealias str = String
+typealias ui8 = UByte
+typealias ui16 = UShort
+typealias ui32 = UInt
+typealias ui64 = ULong
+
+typealias OnlyNull = Nothing?
+typealias Clonable = Cloneable
